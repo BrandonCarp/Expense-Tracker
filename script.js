046b2f28449df857bddfd42f28f079fef46b1d3f
Brandon <69767056+OtakuxCode@users.noreply.github.com>
2021-06-22 21:39:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
 'use strict';
 const table = document.querySelector('table');
-const form = document.querySelector('form');
 let payType = document.querySelector('#payment');
 let name = document.querySelector('#nameInput');
 let date = document.querySelector('#dateInput');
 let amount = document.querySelector('#amountInput');
+
+
+
 const remove = document.createElement('button');
 remove.innerHTML = '<a>x</>';
+
+const userData = userInput => {
+  let td = document.createElement('td');
+  td.innerText = `${userInput.value}`;
+  let tr = document.createElement('tr');
+  tr.appendChild(td);
+  table.appendChild(tr);
+}
+
 
 
 
 document.querySelector('#submitBtn').addEventListener('click', (e) => {
- 
+    userData(payType);
+    userData(name);
+    userData(date);
+    userData(amount);
   
-  let td = document.createElement('td');
-  td.innerText = `${payType.value}`;
-  let tr = document.createElement('tr');
-  tr.appendChild(td);
-
-  td = document.createElement('td');
-  td.innerText = `${name.value}`;
-  tr.appendChild(td);
-
-
-  td = document.createElement('td');
-  td.innerText = `${dateInput.value}`;
-  tr.appendChild(td);
-
-
-
-  td = document.createElement('td');
-  td.innerText = `${amount.value}`;
-  tr.appendChild(td);
-
- const delBtn = function() {
-   td = document.createElement('td');
-   td = remove;
-   tr.appendChild(td);
-   return;
- }
-  delBtn();
-  // td = document.createElement('td');
-  // td = remove;
-  // tr.appendChild(td);
-
-  table.appendChild(tr);
-
-  remove.addEventListener('click', (e) => {
-    tr.remove();
- });
- 
   
 
+//   remove.addEventListener('click', (e) => {
+//     tr.remove();
+//  });
+ 
   e.preventDefault();
 });
+
+
+//   delBtn();
+//   // td = document.createElement('td');
+//   // td = remove;
+//   // tr.appendChild(td);
+
 
 
 
