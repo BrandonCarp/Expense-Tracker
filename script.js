7e60ad0b5307d57495752e16244699592cd5c785
Brandon <69767056+OtakuxCode@users.noreply.github.com>
2021-06-26 20:44:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const currency = document.querySelector('#payment');
 const item = document.querySelector('#name');
 const theDay = document.querySelector('#date');
 const cost = document.querySelector('#amount');
-// Non Inputs
 const tBody = document.querySelector('#tBody');
-const delBtn = document.createElement('button');
 
 
 
@@ -16,37 +14,48 @@ let newRow = function(theCurrency, theItem, theDate, theCost) {
   let itemElement = document.createElement('td');
   let dateElement = document.createElement('td');
   let costElement = document.createElement('td');
+  let delElement = document.createElement('td');
+
+  
 
   currencyElement.textContent = currency.value;
   itemElement.textContent = item.value;
   dateElement.textContent = theDay.value;
   costElement.textContent = cost.value;
+  costElement.style = 'color: red';
 
 
+
+  delElement.innerHTML = `<button>x</button`;
+  delElement.className = 'delBtn';
+  
+ delElement.firstChild.addEventListener('click', e => {
+   rowElement.remove();
+ });
+
+
+ if(item.value != '' && cost.value != '' && theDay.value != '') {
   tBody.appendChild(rowElement);
 
   rowElement.appendChild(currencyElement);
   rowElement.appendChild(itemElement);
   rowElement.appendChild(dateElement);
   rowElement.appendChild(costElement);
-
+  rowElement.appendChild(delElement);
+ } else {
+   alert('Fill out inputs');
+ }
 
 
   item.value = '';
   theDay.value = '';
   cost.value = '';
-
-
-
 }
 
 
 
-
-
-
-
 document.querySelector('#submitBtn').addEventListener('click', (e) => {
+
   newRow(currency.value, item.value, theDay.value, cost.value);
 
 e.preventDefault();
